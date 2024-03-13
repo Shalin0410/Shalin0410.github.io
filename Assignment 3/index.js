2162e5aa4946ed69a8d5384a822d8bf8901eb0f5
Shalin Shah <sashah@usc.edu>
2024-03-13 11:29:58 -0700

    gitignore file

--- Assignment 3/index.js
+++ Assignment 3/index.js
@@ -7,3 +7,16 @@ app.listen(3000, () => console.log("Server listening at port 3000"));
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.get("/search/:tickerSymbol", (req, res) => {
+    tickerSymbol = req.params.tickerSymbol.toUpperCase();
+    res.send("Ticker Symbol: " + tickerSymbol);
+});
+
+app.get("/watchlist", (req, res) => {
+    res.send("Watchlist");
+})
+
+app.get("portfolio", (req, res) => {
+    res.send("Portfolio");
+})
